0fa4901f64a959c7ea8a850aaa3580d5fa166e25
valiancepact <valiancepact@proton.me>
2026-03-06 16:46:37 +0100

    Fix error on startup

--- src/SoftResDataTransformer.lua
+++ src/SoftResDataTransformer.lua
@@ -82,8 +82,10 @@ function M.transform( data )
           roller.sr_plus = 0
           roller.role = roller_role
           table.insert( sr_result[ item_id ].rollers, roller )
-        else if not item.sr_plus_total then --- Only add a roll if sr_plus_total is not present, meaning this is raw SR from SoftRes, not our own custom implementation
-          roller.rolls = roller.rolls + 1
+        else
+          if not item.sr_plus_total then --- Only add a roll if sr_plus_total is not present, meaning this is raw SR from SoftRes, not our own custom implementation
+            roller.rolls = roller.rolls + 1
+          end
         end
       end
     end
